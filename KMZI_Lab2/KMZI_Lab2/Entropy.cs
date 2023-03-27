595facb9ab0629a5872b0d186e9185c24cbebc11
secxndary <valdaitsevv@mail.ru>
2023-03-27 21:57:12 +0300

    вот это да он что делает 2 лабу

---
+++ KMZI_Lab2/KMZI_Lab2/Entropy.cs
@@ -0,0 +1,26 @@
+﻿namespace KMZI_Lab2;
+public class Entropy
+{
+
+    public static double GetShannonEntropy(string s)
+    {
+        var symbolAppearances = new Dictionary<char, int>();
+        foreach (char c in s)
+        {
+            if (!symbolAppearances.ContainsKey(c))
+                symbolAppearances.Add(c, 1);
+            else
+                symbolAppearances[c] += 1;
+        }
+
+        double result = 0.0;
+        int len = s.Length;
+        foreach (var item in symbolAppearances)
+        {
+            var frequency = (double)item.Value / len;
+            result -= frequency * (Math.Log(frequency) / Math.Log(2));
+        }
+
+        return result;
+    }
+}
---
+++ KMZI_Lab2/KMZI_Lab2/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using KMZI_Lab2;
+
+
+Console.WriteLine(Entropy.GetShannonEntropy("hello world"));
