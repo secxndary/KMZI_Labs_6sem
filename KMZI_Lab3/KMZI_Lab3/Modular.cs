0f79bcea8774e9c9d19fadbb8884830f1fef9e2e
secxndary <valdaitsevv@mail.ru>
2023-04-06 22:31:34 +0300

    3 лаба вроде почти все

---
+++ KMZI_Lab3/KMZI_Lab3/Modular.cs
@@ -0,0 +1,75 @@
+﻿namespace KMZI_Lab3;
+
+public class Modular
+{
+    // Алгоритм Евклида для НОД
+    public static ulong GetGCD(ulong a, ulong b)
+    {
+        if (a == 0)             return b;
+        if (b == 0)             return a;
+        if (a == 1 || b == 1)   return 1;
+
+        while (a != 0 &&  b != 0)
+        {
+            if (a > b)  
+                a -= b;
+            else        
+                b -= a;
+        }
+
+        return (a > b) ? a : b;
+    }
+
+
+    // Получить простые числа от 2 до n
+    public static List<ulong> GetPrimes(ulong n)
+    {
+        var numbers = new List<ulong>();
+        for (var i = 2u; i <= n; i++)
+            numbers.Add(i);
+
+        for (var i = 0; i < numbers.Count; i++)
+            for (var j = 2u; numbers[i] * j <= n; j++)
+                numbers.Remove(numbers[i] * j);
+
+        return numbers;
+    }
+
+
+    // Получить простые числа от m до n
+    public static List<ulong> GetPrimes(ulong m, ulong n)
+    {
+        if (m > n)
+            (m, n) = (n, m);
+
+        var isPrime = new bool[n - m + 1];
+        for (var i = 0u; i < isPrime.Length; i++)
+            isPrime[i] = true;
+
+        for (var i = 2u; i * i <= n; i++)
+            for (var j = (m + i - 1) / i * i; j <= n; j += i)
+                if (j != i && j >= m)
+                    isPrime[j - m] = false;
+
+        var primes = new List<ulong>();
+        for (ulong i = m; i <= n; i++)
+        {
+            if (i < 2) continue;
+            if (isPrime[i - m])
+                primes.Add(i);
+        }
+
+        return primes;
+    }
+
+
+    // Получить количество простых чисел
+    public static int GetPrimesCount(ulong n) => GetPrimes(n).Count;
+    
+    public static int GetPrimesCount(ulong m, ulong n) => GetPrimes(m, n).Count;
+
+
+    // Получить примерное количество простых чисел [n / ln(n)]
+    // Для общего развития: это формула из Теоремы о распределении простых чисел
+    public static double GetApproximatePrimesCount(ulong n) => Math.Round(n / Math.Log(n), 1);
+}
---
+++ KMZI_Lab3/KMZI_Lab3/Program.cs
@@ -0,0 +1,16 @@
+﻿using KMZI_Lab3;
+const ulong m = 421;
+const ulong n = 457;
+
+Console.WriteLine($"НОД ({m}, {n}) = {Modular.GetGCD(m, n)}");
+Console.WriteLine($"НОД (4, 8, 32) = {Modular.GetGCD(4, Modular.GetGCD(32, 8))}");
+
+Console.WriteLine($"\nПростые числа от 2 до {n} ({Modular.GetPrimesCount(n)} чисел):");
+foreach (var item in Modular.GetPrimes(n))
+    Console.Write($"{item} ");
+
+Console.WriteLine($"\n\nПростые числа от {m} до {n} ({Modular.GetPrimesCount(m, n)} чисел):");
+foreach (var item in Modular.GetPrimes(m, n))
+    Console.Write($"{item} ");
+
+Console.WriteLine($"\n\n{n} / ln({n}) = {Modular.GetApproximatePrimesCount(n)}");
