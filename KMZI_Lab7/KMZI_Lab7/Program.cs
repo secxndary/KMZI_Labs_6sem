bd5652fe23fe14ee1326f1f301c01ffa735b951b
secxndary <valdaitsevv@mail.ru>
2023-05-02 12:18:49 +0300

    СЦКА БЛЯДЬ ХУЙНЯ ЕБАНАЯ НАЗУЙ

--- KMZI_Lab7/KMZI_Lab7/Program.cs
+++ KMZI_Lab7/KMZI_Lab7/Program.cs
@@ -8,13 +8,44 @@ var plainText = CypherHelper.GetOpenText();
 int totalBits, changedBits;
 
 
-var encryptedText = Cypher.EncryptEEE2(plainText, firstKey, secondKey, out changedBits);
-CypherHelper.WriteToFile(encryptedText, fileNameEncrypt);
+//var encryptedText = Cypher.EncryptDES(plainText, firstKey, out changedBits);
+//CypherHelper.WriteToFile(encryptedText, fileNameEncrypt);
 
-var decryptedText = Cypher.DecryptEEE2(encryptedText, firstKey, secondKey);
-CypherHelper.WriteToFile(decryptedText, fileNameDecrypt);
+//var decryptedText = Cypher.DecryptDES(encryptedText, firstKey);
+//CypherHelper.WriteToFile(decryptedText, fileNameDecrypt);
 
+//Console.WriteLine();
+//Console.WriteLine($"Total bits count:\t{totalBits = CypherHelper.GetTotalBits(plainText)} bits");
+//Console.WriteLine($"Avalanche Effect:\t{changedBits} bits (changed)");
+//Console.WriteLine($"Percentage ratio:\t{CypherHelper.GetPercentageRatio(totalBits, changedBits)}%");
+
+
+
+
+
+int changedPlainToEncrypt;
+var cypherTextInitial = CypherHelper.GetBytes("Ъ{КК\aц!(Bђu!r\r\n\u0014Е»W Џ—ОК%");
+var key = CypherHelper.GetBytes("myKeyDES");
+var newEncryptedText = Cypher.EncryptDES(cypherTextInitial, key, out changedPlainToEncrypt);
+CypherHelper.WriteToFile(newEncryptedText, fileNameEncrypt);
 Console.WriteLine();
-Console.WriteLine($"Total bits count:\t{totalBits = CypherHelper.GetTotalBits(plainText)} bits");
-Console.WriteLine($"Avalanche Effect:\t{changedBits} bits (changed)");
-Console.WriteLine($"Percentage ratio:\t{CypherHelper.GetPercentageRatio(totalBits, changedBits)}%");
+Console.WriteLine($"Total bits count:\t{totalBits = CypherHelper.GetTotalBits(newEncryptedText)} bits");
+Console.WriteLine($"Avalanche Effect:\t{changedPlainToEncrypt} bits (changed)");
+Console.WriteLine($"Percentage ratio:\t{CypherHelper.GetPercentageRatio(totalBits, changedPlainToEncrypt)}%");
+
+
+
+
+
+
+
+//var encryptedText = Cypher.EncryptDES(plainText, key, out changedPlainToEncrypt);
+//CypherHelper.WriteToFile(encryptedText, fileNameEncrypt);
+
+//var decryptedText = Cypher.DecryptDES(encryptedText, key);
+//CypherHelper.WriteToFile(decryptedText, fileNameDecrypt);
+
+//Console.WriteLine();
+//Console.WriteLine($"Total bits count:\t{totalBits = CypherHelper.GetTotalBits(plainText)} bits");
+//Console.WriteLine($"Avalanche Effect:\t{changedBits} bits (changed)");
+//Console.WriteLine($"Percentage ratio:\t{CypherHelper.GetPercentageRatio(totalBits, changedBits)}%");
