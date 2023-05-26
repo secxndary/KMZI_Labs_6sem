7fc6d511cbe912782b021ef6c4f2ce4c5bb21980
alexander valdaitsevv <valdaitsevv@mail.ru>
2023-05-26 10:52:59 +0300

    Lab 9: generate super increasing sequence

---
+++ KMZI_Lab9/KMZI_Lab9/Cypher.cs
@@ -0,0 +1,40 @@
+﻿using System.Numerics;
+namespace KMZI_Lab9;
+
+public class Cypher
+{
+    public static List<BigInteger> GenerateSuperIncreasingSequence(BigInteger initialTerm, int z)
+    {
+        List<BigInteger> sequence = new List<BigInteger>();
+        BigInteger term = initialTerm;
+
+        for (int i = 0; i < z; i++)
+        {
+            sequence.Add(term);
+            term += initialTerm;
+            initialTerm <<= 1;
+        }
+
+        return sequence;
+    }
+
+
+
+    public static BigInteger GenerateRandomNumber(int n)
+    {
+        Random random = new Random();
+        byte[] bytes = new byte[n / 8];
+        random.NextBytes(bytes);
+
+        // Добавляем биты нуля, чтобы число было точно размером n бит
+        int numExtraBits = n % 8;
+        if (numExtraBits > 0)
+        {
+            byte extraBitsMask = (byte)(0xFF >> (8 - numExtraBits));
+            bytes[0] &= extraBitsMask;
+        }
+
+        BigInteger randomNumber = new BigInteger(bytes);
+        return randomNumber;
+    }
+}
---
+++ KMZI_Lab9/KMZI_Lab9/Program.cs
@@ -0,0 +1,11 @@
+﻿using KMZI_Lab9;
+
+var initialTerm = Cypher.GenerateRandomNumber(130);
+int z = 6;
+var sequence = Cypher.GenerateSuperIncreasingSequence(initialTerm, z);
+
+Console.WriteLine("Super increasing sequence:");
+foreach (var term in sequence)
+{
+    Console.WriteLine(term);
+}
