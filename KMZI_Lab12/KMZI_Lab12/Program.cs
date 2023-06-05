8aed583ce66a826bb469e1340b111c0eeb244950
alexander valdaitsevv <valdaitsevv@mail.ru>
2023-06-05 16:18:08 +0300

    Lab12 v1.0 RELEASE

--- KMZI_Lab12/KMZI_Lab12/Program.cs
+++ KMZI_Lab12/KMZI_Lab12/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Numerics;
+﻿using System.Diagnostics;
+using System.Numerics;
 using KMZI_Lab12;
 
 const string fileNameSignatureRSA = "signature_rsa.txt";
@@ -8,45 +9,60 @@ var openText = CypherHelper.GetOpenText();
 
 
 
+
 // Генерация и верификация ЭЦП на основе алгоритма RSA
+var stopWatchRSA = new Stopwatch();
+stopWatchRSA.Start();
+
 byte[] rsaPublicKey;
 byte[] rsaPrivateKey;
 
 RSACypher.GenerateRSAKeys(out rsaPublicKey, out rsaPrivateKey);
-byte[] rsaSignature = RSACypher.CreateRSASignature(openText, rsaPrivateKey);
-bool rsaIsValid = RSACypher.VerifyRSASignature(openText, rsaSignature, rsaPublicKey);
-
+var rsaSignature = RSACypher.CreateRSASignature(openText, rsaPrivateKey);
+var rsaIsValid = RSACypher.VerifyRSASignature(openText, rsaSignature, rsaPublicKey);
 CypherHelper.WriteToFile(rsaSignature, fileNameSignatureRSA);
-Console.WriteLine("Is valid signature RSA:\t\t" + rsaIsValid);
+
+stopWatchRSA.Stop();
+Console.WriteLine($"RSA signature:\t\t{stopWatchRSA.ElapsedTicks} ticks ({stopWatchRSA.ElapsedMilliseconds} ms)");
+Console.WriteLine($"Is valid RSA: \t\t{rsaIsValid}\n");
 
 
 
 
 // Генерация и верификация ЭЦП на основе алгоритма Эль-Гамаля
+var stopWatchElGamal = new Stopwatch();
+stopWatchElGamal.Start();
+
 int p = 2137;  
 int g = 2127; 
 int x = 1116; 
 int k = 7; 
 int H = 2119;
 
-byte[] signatureElGamal = ElGamalCypher.GenerateElGamalSignature(p, g, x, k, H);
-int y = (int)BigInteger.ModPow(g, x, p);
-bool isVerified = ElGamalCypher.VerifyElGamalSignature(p, g, y, H, signatureElGamal);
-
+var signatureElGamal = ElGamalCypher.GenerateElGamalSignature(p, g, x, k, H);
+var y = (int)BigInteger.ModPow(g, x, p);
+var elGamalIsValid = ElGamalCypher.VerifyElGamalSignature(p, g, y, H, signatureElGamal);
 CypherHelper.WriteToFile(signatureElGamal, fileNameSignatureElGamal);
-Console.WriteLine("Is valid signature El-Gamal:\t" + isVerified);
 
+stopWatchElGamal.Stop();
+Console.WriteLine($"El-Gamal signature:\t{stopWatchElGamal.ElapsedTicks} ticks ({stopWatchElGamal.ElapsedMilliseconds} ms)");
+Console.WriteLine($"Is valid El-Gamal: \t{elGamalIsValid}\n");
 
 
 
 
 // Генерация и верификация ЭЦП на основе алгоритма Шнорра
+var stopWatchSchnorr = new Stopwatch();
+stopWatchSchnorr.Start();
+
 byte[] schnorrPublicKey;
 byte[] schnorrPrivateKey;
 
 SchnorrCypher.GenerateSchnorrKeys(out schnorrPublicKey, out schnorrPrivateKey);
-byte[] signatureSchnorr = SchnorrCypher.CreateSchnorrSignature(openText, schnorrPrivateKey);
-bool schnorrIsValid = SchnorrCypher.VerifySchnorrSignature(openText, signatureSchnorr, schnorrPublicKey);
-
+var signatureSchnorr = SchnorrCypher.CreateSchnorrSignature(openText, schnorrPrivateKey);
+var schnorrIsValid = SchnorrCypher.VerifySchnorrSignature(openText, signatureSchnorr, schnorrPublicKey);
 CypherHelper.WriteToFile(signatureSchnorr, fileNameSignatureSchnorr);
-Console.WriteLine("Is valid signature Schnorr:\t" + schnorrIsValid);
+
+stopWatchSchnorr.Stop();
+Console.WriteLine($"Schnorr signature:\t{stopWatchSchnorr.ElapsedTicks} ticks ({stopWatchSchnorr.ElapsedMilliseconds} ms)");
+Console.WriteLine($"Is valid Schnorr: \t{schnorrIsValid}");
