e3cd5e1c47a8ede1544aaca785c11f8bd0dfc652
alexander valdaitsevv <valdaitsevv@mail.ru>
2023-06-05 12:30:16 +0300

    Runtime graphics Lab11

--- KMZI_Lab11/KMZI_Lab11/Program.cs
+++ KMZI_Lab11/KMZI_Lab11/Program.cs
@@ -1,10 +1,10 @@
 ﻿using KMZI_Lab11;
 const string fileNameSHA1 = "hash_sha1.txt";
-const string fileNameMD5 = "hash_md5.txt";
+const string fileNameMD5  = "hash_md5.txt";
 
 var opentext = CypherHelper.GetOpenText();
 var hashSHA1 = SHA1Hash.GetSHA1Hash(opentext);
-var hashMD5 = MD5Hash.GetMD5Hash(opentext);
+var hashMD5  = MD5Hash.GetMD5Hash(opentext);
 
 CypherHelper.WriteToFile(hashSHA1, fileNameSHA1);
 CypherHelper.WriteToFile(hashMD5, fileNameMD5);
