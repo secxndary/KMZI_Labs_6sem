6c119faad9376bbfc742139427c956389f483d29
alexander valdaitsevv <valdaitsevv@mail.ru>
2023-06-06 19:05:17 +0300

    Lab13 task 1.1 completed

---
+++ KMZI_Lab13/KMZI_Lab13/EC.cs
@@ -0,0 +1,16 @@
+﻿namespace KMZI_Lab13;
+
+
+public class EC
+{
+    public static List<(int, double)> GetPoints(int xMin, int xMax, int a, int b, int p)
+    {
+        var points = new List<(int, double)>();
+        for (int x = xMin; x <= xMax; x++)
+        {
+            double y = Math.Round(Math.Sqrt((x * x * x + a * x + b) % p), 2);
+            points.Add((x, y));
+        }
+        return points;
+    }
+}
---
+++ KMZI_Lab13/KMZI_Lab13/Program.cs
@@ -0,0 +1,16 @@
+﻿using KMZI_Lab13;
+
+var a = -1;
+var b = 1;
+var p = 751;
+
+var xMin = 106;
+var xMax = 140;
+
+
+// Задание 1.1
+var points = EC.GetPoints(xMin, xMax, a, b, p);
+foreach (var point in points)
+    Console.WriteLine($"x = {point.Item1}, y = {point.Item2}");
+
+// Задание 1.2
