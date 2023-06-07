cb39d4855663838229d7011e8b83a0801ac45b6b
alexander valdaitsevv <valdaitsevv@mail.ru>
2023-06-07 12:30:41 +0300

    Lab14 v1.0 RELEASE

--- KMZI_Lab14/KMZI_Lab14/Program.cs
+++ KMZI_Lab14/KMZI_Lab14/Program.cs
@@ -1,29 +1,33 @@
 ﻿using System;
 
-
 namespace KMZI_Lab14
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            var inp = @"C:\Users\valda\source\repos\semester#6\КМЗИ\KMZI_Lab14\KMZI_Lab14\Images\sample.bmp";
-            var res = @"C:\Users\valda\source\repos\semester#6\КМЗИ\KMZI_Lab14\KMZI_Lab14\Images\ecnrypted_by_rows.bmp";
-            Steganography.EmbedMessage(inp, "ValdaitsevAlexanderDenisovich", res);
-            Console.WriteLine("Embed completed");
-            var resText = Steganography.ExtractMessage(res);
-            Console.WriteLine($"Result text: {resText}");
+            var fileNameOpen = @"C:\Users\valda\source\repos\semester#6\КМЗИ\KMZI_Lab14\KMZI_Lab14\Images\sample.bmp";
+            var fileNameEncryptByRows = @"C:\Users\valda\source\repos\semester#6\КМЗИ\KMZI_Lab14\KMZI_Lab14\Images\ecnrypted_by_rows.bmp";
+            var fileNameEncryptByColumns = @"C:\Users\valda\source\repos\semester#6\КМЗИ\KMZI_Lab14\KMZI_Lab14\Images\ecnrypted_by_columns.bmp";
+            var fileNameMatrixSample = @"C:\Users\valda\source\repos\semester#6\КМЗИ\KMZI_Lab14\KMZI_Lab14\Images\matrix_sample.bmp";
+            var fileNameMatrixEncryptByRows = @"C:\Users\valda\source\repos\semester#6\КМЗИ\KMZI_Lab14\KMZI_Lab14\Images\matrix_encrypt.bmp";
+            var fileNameMatrixEncryptByColumns = @"C:\Users\valda\source\repos\semester#6\КМЗИ\KMZI_Lab14\KMZI_Lab14\Images\matrix_encrypt_columns.bmp";
 
-            Steganography2.EmbedMessage(inp, "Valdaitsev", @"C:\Users\valda\source\repos\semester#6\КМЗИ\KMZI_Lab14\KMZI_Lab14\Images\ecnrypted_by_columns.bmp");
-            Console.WriteLine($"Result text: {Steganography2.ExtractMessage(@"C:\Users\valda\source\repos\semester#6\КМЗИ\KMZI_Lab14\KMZI_Lab14\Images\ecnrypted_by_columns.bmp")}");
+            var openTextByRows = "ValdaitsevAlexanderDenisovich";
+            var openTextByColumns = "Valdaitsev";
 
-            /*
-            RandomLSB.EmbedMessage(inp, "Super cool message for embed",  @"C:\course\leet\app\lab14\res2.bmp");
-            Console.WriteLine($"Res2: {RandomLSB.ExtractMessage(@"C:\course\leet\app\lab14\res2.bmp")}");
-            */
 
-            Steganography.SaveLastBit(inp, @"C:\Users\valda\source\repos\semester#6\КМЗИ\KMZI_Lab14\KMZI_Lab14\Images\matrix_sample.bmp");
-            Steganography.SaveLastBit(res, @"C:\Users\valda\source\repos\semester#6\КМЗИ\KMZI_Lab14\KMZI_Lab14\Images\matrix_encrypt.bmp");
+            Steganography.HideMessageByRows(fileNameOpen, openTextByRows, fileNameEncryptByRows);
+            Steganography.HideMessageByColumns(fileNameOpen, openTextByColumns, fileNameEncryptByColumns);
+            var resultByRows = Steganography.ExtractMessageByRows(fileNameEncryptByRows);
+            var resultByColumns = Steganography.ExtractMessageByColumns(fileNameEncryptByColumns);
+           
+            Console.WriteLine($"Text by rows: {resultByRows}");
+            Console.WriteLine($"Text by columns: {resultByColumns}");
+
+            Steganography.GetColorMatrix(fileNameOpen, fileNameMatrixSample);
+            Steganography.GetColorMatrix(fileNameEncryptByRows, fileNameMatrixEncryptByRows);
+            Steganography.GetColorMatrix(fileNameEncryptByColumns, fileNameMatrixEncryptByColumns);
         }
     }
 }
--- KMZI_Lab14/KMZI_Lab14/Steganography.cs
+++ KMZI_Lab14/KMZI_Lab14/Steganography.cs
@@ -9,7 +9,7 @@ namespace KMZI_Lab14
 {
     public class Steganography
     {
-        public static void EmbedMessage(string containerPath, string message, string outputImagePath)
+        public static void HideMessageByRows(string containerPath, string message, string outputImagePath)
         {
             Bitmap container = new Bitmap(containerPath);
 
@@ -83,7 +83,7 @@ namespace KMZI_Lab14
 
 
 
-        public static string ExtractMessage(string imagePath)
+        public static string ExtractMessageByRows(string imagePath)
         {
             Bitmap container = new Bitmap(imagePath);
 
@@ -143,10 +143,145 @@ namespace KMZI_Lab14
 
 
 
-        public static void SaveLastBit(string imagePath, string outputPath)
+
+        public static void HideMessageByColumns(string containerPath, string message, string outputImagePath)
+        {
+            Bitmap container = new Bitmap(containerPath);
+
+            byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+            BitArray messageBits = new BitArray(messageBytes);
+
+            int maxMessageBits = (container.Width * container.Height) * 3;
+            if (messageBits.Length > maxMessageBits)
+                throw new Exception("Сообщение слишком большое для данного контейнера");
+
+            int messageBitIndex = 0;
+            for (int y = 0; y < container.Width; y++)
+            {
+                for (int x = 0; x < container.Height; x++)
+                {
+                    Color pixel = container.GetPixel(x, y);
+                    if (messageBitIndex < messageBits.Length)
+                    {
+                        BitArray redBits = new BitArray(new byte[] { pixel.R });
+                        BitArray greenBits = new BitArray(new byte[] { pixel.G });
+                        BitArray blueBits = new BitArray(new byte[] { pixel.B });
+
+                        if (messageBits[messageBitIndex])
+                            redBits[0] = true;
+                        else
+                            redBits[0] = false;
+
+                        messageBitIndex++;
+
+                        if (messageBitIndex < messageBits.Length)
+                        {
+                            if (messageBits[messageBitIndex])
+                                greenBits[0] = true;
+                            else
+                                greenBits[0] = false;
+
+                            messageBitIndex++;
+                        }
+
+                        if (messageBitIndex < messageBits.Length)
+                        {
+                            if (messageBits[messageBitIndex])
+                                blueBits[0] = true;
+                            else
+                                blueBits[0] = false;
+
+                            messageBitIndex++;
+                        }
+
+                        byte[] newRedBytes = new byte[1];
+                        byte[] newGreenBytes = new byte[1];
+                        byte[] newBlueBytes = new byte[1];
+                        redBits.CopyTo(newRedBytes, 0);
+                        greenBits.CopyTo(newGreenBytes, 0);
+                        blueBits.CopyTo(newBlueBytes, 0);
+                        Color newPixel = Color.FromArgb(newRedBytes[0], newGreenBytes[0], newBlueBytes[0]);
+
+                        container.SetPixel(y, x, newPixel);
+                    }
+                    else
+                        break;
+                }
+
+                if (messageBitIndex >= messageBits.Length)
+                    break;
+            }
+
+            container.Save(outputImagePath, ImageFormat.Png);
+        }
+
+
+
+        public static string ExtractMessageByColumns(string imagePath)
+        {
+            Bitmap container = new Bitmap(imagePath);
+            int messageBitsLength = (container.Width * container.Height) * 3;
+            BitArray messageBits = new BitArray(messageBitsLength);
+
+            int messageBitIndex = 0;
+            for (int y = 0; y < container.Width; y++)
+            {
+                for (int x = 0; x < container.Height; x++)
+                {
+                    Color pixel = container.GetPixel(y, x);
+                    BitArray redBits = new BitArray(new byte[] { pixel.R });
+                    BitArray greenBits = new BitArray(new byte[] { pixel.G });
+                    BitArray blueBits = new BitArray(new byte[] { pixel.B });
+
+                    bool redBit = redBits[0];
+                    messageBits[messageBitIndex] = redBit;
+                    messageBitIndex++;
+
+                    if (messageBitIndex < messageBitsLength)
+                    {
+                        bool greenBit = greenBits[0];
+                        messageBits[messageBitIndex] = greenBit;
+                        messageBitIndex++;
+                    }
+
+                    if (messageBitIndex < messageBitsLength)
+                    {
+                        bool blueBit = blueBits[0];
+                        messageBits[messageBitIndex] = blueBit;
+                        messageBitIndex++;
+                    }
+
+                    if (messageBitIndex >= messageBitsLength)
+                        break;
+                }
+
+                if (messageBitIndex >= messageBitsLength)
+                    break;
+            }
+
+            byte[] messageBytes = new byte[(messageBits.Length + 7) / 8];
+            messageBits.CopyTo(messageBytes, 0);
+            string message = Encoding.UTF8.GetString(messageBytes);
+
+            var sb = new StringBuilder("");
+            foreach (var ch in message)
+            {
+                if (ch > 255)
+                    break;
+                sb.Append(ch);
+            }
+
+            return sb.ToString();
+        }
+
+
+
+
+
+
+        public static void GetColorMatrix(string imagePath, string outputPath)
         {
             Bitmap originalImage = new Bitmap(imagePath);
-
             Bitmap newImage = new Bitmap(originalImage.Width, originalImage.Height);
 
             for (int y = 0; y < originalImage.Height; y++)
